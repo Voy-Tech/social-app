103f309a9aa2dbc497a74ca44f4a047116b3c5e0
Wojciech Prokopek <w.prokopek@gmail.com>
2022-10-20 20:03:35 +0200

    implement VideoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import axios from "axios";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Social App" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No videos"} />
+      )}
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
